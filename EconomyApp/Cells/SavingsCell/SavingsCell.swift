20b076280cc5958d5e59d394aa4aeb77e53fbe41
SaYan4ik <66200306+SaYan4ik@users.noreply.github.com>
2023-02-03 15:02:27 +0300

    set settings for SavingsCell

--- EconomyApp/Cells/SavingsCell/SavingsCell.swift
+++ EconomyApp/Cells/SavingsCell/SavingsCell.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class SavingsCell: UICollectionViewCell {
 
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var typeImage: UIImageView!
+    @IBOutlet weak var amounLabel: UILabel!
+    
+    static var id = String(describing: SavingsCell.self)
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func set(savings: SavingsModel) {
+        guard let imageNew = savings.typeValue?.image else { return }
+        guard let image : UIImage = UIImage(data: imageNew)?.withRenderingMode(.alwaysTemplate) else { return }
+        self.typeImage.image = image
+        
+        self.amounLabel.text = savings.currency
+        self.container.layer.cornerRadius = 12
     }
 
 }
--- EconomyApp/Controllers/SavingAndAssetsController/SavingAndAssetsController.swift
+++ EconomyApp/Controllers/SavingAndAssetsController/SavingAndAssetsController.swift
@@ -10,14 +10,37 @@ import UIKit
 class SavingAndAssetsController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
+    private var allSavings = [SavingsModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
+        setupCollection()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.setGradientBackground()
+        getData()
+    }
+    
+    
+    private func setupCollection() {
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        collectionView.contentInset = UIEdgeInsets(top: 10, left: 5, bottom: 10, right: 5)
+        registrationCell()
+        
+    }
+    
+    private func registrationCell() {
+        let nib = UINib(nibName: SavingsCell.id, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: SavingsCell.id)
+    }
+    
+    private func getData() {
+        self.allSavings = RealmManager<SavingsModel>().read()
+        self.collectionView.reloadData()
     }
     
     private func setupNavBar() {
@@ -34,5 +57,22 @@ class SavingAndAssetsController: UIViewController {
     }
     
     
+}
+
+extension SavingAndAssetsController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
     
 }
+
+extension SavingAndAssetsController: UICollectionViewDelegate {
+    
+}
+
+
